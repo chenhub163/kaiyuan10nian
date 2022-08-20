3ee24a734a38ef449be3802f4c71ac6737bfa706
chenhub163 <chen_hub@163.com>
2022-08-20 14:49:20 +0800

    vite项目配置

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,13 @@
 </script>
 
 <template>
-  <h1>前端项目：开源十年</h1>
+  <h1 class="color">前端项目：开源十年</h1>
   <span>kaiyuan10nian</span>
 </template>
 
 <style scoped>
-
+.color {
+  display: flex;
+  color: red;
+}
 </style>
